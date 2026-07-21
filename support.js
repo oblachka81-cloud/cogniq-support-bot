6da97c2fc6e97a364dddcc56628dd70810eb044c
oblachka81-cloud <oblachka81@gmail.com>
2026-07-22 07:39:30 +0800

    Change HTTP server to async function
    Updated the HTTP server to use an asynchronous function.

--- support.js
+++ support.js
@@ -67,7 +67,7 @@ bot.start(async (ctx) => {
   });
 });
 
-http.createServer((req, res) => {
+http.createServer(async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
